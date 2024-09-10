0eff00ab959cd05fd5ebc0266ae08c5f794ed0a8
rawan-salah137 <144793515+rawan-salah137@users.noreply.github.com>
2024-09-10 18:19:49 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,57 @@
+function ans1() {
+    const plus = document.querySelector(".plus1");
+    const ans = document.querySelector(".ans1");
+
+    if (plus.classList.contains("fa-circle-plus")) {
+        plus.classList.remove("fa-circle-plus");
+        plus.classList.add("fa-circle-minus");
+        ans.style.display = "block";
+    } else if (plus.classList.contains("fa-circle-minus")) {
+        plus.classList.remove("fa-circle-minus");
+        plus.classList.add("fa-circle-plus");
+        ans.style.display = "none";
+    }
+}
+
+function ans2() {
+    const plus = document.querySelector(".plus2");
+    const ans = document.querySelector(".ans2");
+
+    if (plus.classList.contains("fa-circle-plus")) {
+        plus.classList.remove("fa-circle-plus");
+        plus.classList.add("fa-circle-minus");
+        ans.style.display = "block";
+    } else if (plus.classList.contains("fa-circle-minus")) {
+        plus.classList.remove("fa-circle-minus");
+        plus.classList.add("fa-circle-plus");
+        ans.style.display = "none";
+    }
+}
+function ans3() {
+    const plus = document.querySelector(".plus3");
+    const ans = document.querySelector(".ans3");
+
+    if (plus.classList.contains("fa-circle-plus")) {
+        plus.classList.remove("fa-circle-plus");
+        plus.classList.add("fa-circle-minus");
+        ans.style.display = "block";
+    } else if (plus.classList.contains("fa-circle-minus")) {
+        plus.classList.remove("fa-circle-minus");
+        plus.classList.add("fa-circle-plus");
+        ans.style.display = "none";
+    }
+}
+function ans4() {
+    const plus = document.querySelector(".plus4");
+    const ans = document.querySelector(".ans4");
+
+    if (plus.classList.contains("fa-circle-plus")) {
+        plus.classList.remove("fa-circle-plus");
+        plus.classList.add("fa-circle-minus");
+        ans.style.display = "block";
+    } else if (plus.classList.contains("fa-circle-minus")) {
+        plus.classList.remove("fa-circle-minus");
+        plus.classList.add("fa-circle-plus");
+        ans.style.display = "none";
+    }
+}
